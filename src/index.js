4ba4c641c012c7339d288972fd71dd4f557959b7
Luis Chávez <luischdu@gmail.com>
2021-06-04 09:59:14 -0500

    auth

--- src/index.js
+++ src/index.js
@@ -2,9 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import AppRoutes from "./routes/AppRoutes";
 
+import { Provider } from "react-redux";
+import { store } from "./store/store";
+
 ReactDOM.render(
   <React.StrictMode>
+     <Provider store={store}>
     <AppRoutes />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
